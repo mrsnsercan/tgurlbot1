e2519d8abd047e9a2c2e2f5c255458105eb29a27
mrsnsercan <130498730+mrsnsercan@users.noreply.github.com>
2023-12-18 01:37:06 +0300

    Update ytdlp_button.py

--- plugins/ytdlp_button.py
+++ plugins/ytdlp_button.py
@@ -274,7 +274,7 @@ async def yt_dlp_call_back(bot, update):
     if ".lat" in yt_dlp_url:
         command_to_exec.append("--referer")
         command_to_exec.append("https://vidmoly.to/")
-    if "dizigom" in yt_dlp_url:
+    if "gomindex" in yt_dlp_url:
         command_to_exec.append("--referer")
         command_to_exec.append("https://play.dizigom1.com/")
     if "https://upstreamcdn.co" in yt_dlp_url:
